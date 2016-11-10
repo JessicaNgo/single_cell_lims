ef7be954dd157795ac928ee2d20e0653cf3c5e83
jtaghiyar <jtaghiyar@bccrc.ca>
2016-11-10 13:52:13 -0800

    minor updates.

--- apps/core/models.py
+++ apps/core/models.py
@@ -278,7 +278,7 @@ class Library(models.Model, FieldValue, LibraryAssistant):
     relates_to = models.ManyToManyField(
         "self",
         verbose_name="Relates to",
-        null=True,
+        # null=True,
         blank=True,
         )
 
@@ -712,7 +712,7 @@ class Sequencing(models.Model, FieldValue):
     relates_to = models.ManyToManyField(
         "self",
         verbose_name="Relates to",
-        null=True,
+        # null=True,
         blank=True,
         )
 
